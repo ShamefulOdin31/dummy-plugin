03a35a75ac876aa91eda034f7bb61af36007fdf5
ShamefulOdin31 <61662359+ShamefulOdin31@users.noreply.github.com>
2021-02-16 09:50:00 -0600

    added attempt for new nodegraph

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,13 +1,15 @@
-import { QueryResponse } from './types';
-
+//import { QueryResponse } from './types';
+import { NodeGraphDataFrameFieldNames } from '@grafana/ui';
 import {
   DataQueryRequest,
   DataQueryResponse,
   DataSourceApi,
   DataSourceInstanceSettings,
+  FieldColorModeId,
   FieldType,
   MutableDataFrame,
-  toDataFrame,
+  ArrayVector
+ // toDataFrame,
 } from '@grafana/data';
 
 import { MyQuery, MyDataSourceOptions } from './types';
@@ -18,8 +20,8 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   }
 
   // Code for dataframes
-  /*
-  async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
+  
+  /*async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
     const promises = options.targets.map((query) =>{
       var timeValues :any
       var actualValues :any
@@ -62,8 +64,193 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     return Promise.all(promises).then((data) => ({ data }))
   }*/
 
-  // Code for topology
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
+    const promises = options.targets.map((query) =>{
+      const idField = {
+        name: NodeGraphDataFrameFieldNames.id,
+        type: FieldType.string,
+        values: new ArrayVector(),
+      };
+      const titleField = {
+        name: NodeGraphDataFrameFieldNames.title,
+        type: FieldType.string,
+        values: new ArrayVector(),
+        config: { displayName: 'Name' },
+      };
+    
+      const typeField = {
+        name: NodeGraphDataFrameFieldNames.subTitle,
+        type: FieldType.string,
+        values: new ArrayVector(),
+        config: { displayName: 'Type' },
+      };
+    
+      const mainStatField = {
+        name: NodeGraphDataFrameFieldNames.mainStat,
+        type: FieldType.number,
+        values: new ArrayVector(),
+        config: { unit: 'ms/t', displayName: 'Average response time' },
+      };
+    
+      const secondaryStatField = {
+        name: NodeGraphDataFrameFieldNames.secondaryStat,
+        type: FieldType.string,
+        values: new ArrayVector(),
+        config: { displayName: 'Requests count' },
+      };
+    
+      const successField = {
+        name: NodeGraphDataFrameFieldNames.arc + 'success',
+        type: FieldType.number,
+        values: new ArrayVector(),
+        config: { color: { fixedColor: 'green', mode: FieldColorModeId.Fixed } },
+      };
+    
+      const errorsField = {
+        name: NodeGraphDataFrameFieldNames.arc + 'errors',
+        type: FieldType.number,
+        values: new ArrayVector(),
+        config: { color: { fixedColor: 'semi-dark-yellow', mode: FieldColorModeId.Fixed } },
+      };
+    
+      const faultsField = {
+        name: NodeGraphDataFrameFieldNames.arc + 'faults',
+        type: FieldType.number,
+        values: new ArrayVector(),
+        config: { color: { fixedColor: 'red', mode: FieldColorModeId.Fixed } },
+      };
+    
+      const throttledField = {
+        name: NodeGraphDataFrameFieldNames.arc + 'throttled',
+        type: FieldType.number,
+        values: new ArrayVector(),
+        config: { color: { fixedColor: 'purple', mode: FieldColorModeId.Fixed } },
+      };
+    
+      const edgeIdField = {
+        name: NodeGraphDataFrameFieldNames.id,
+        type: FieldType.string,
+        values: new ArrayVector(),
+      };
+      const edgeSourceField = {
+        name: NodeGraphDataFrameFieldNames.source,
+        type: FieldType.string,
+        values: new ArrayVector(),
+      };
+      const edgeTargetField = {
+        name: NodeGraphDataFrameFieldNames.target,
+        type: FieldType.string,
+        values: new ArrayVector(),
+      };
+    
+      // These are needed for links to work
+      const edgeSourceNameField = {
+        name: 'sourceName',
+        type: FieldType.string,
+        values: new ArrayVector(),
+      };
+      const edgeTargetNameField = {
+        name: 'targetName',
+        type: FieldType.string,
+        values: new ArrayVector(),
+      };
+
+      const edgeMainStatField = {
+        name: NodeGraphDataFrameFieldNames.mainStat,
+        type: FieldType.string,
+        values: new ArrayVector(),
+        config: { displayName: 'Response percentage' },
+      };
+
+      const edgeSecondaryStatField = {
+        name: NodeGraphDataFrameFieldNames.secondaryStat,
+        type: FieldType.string,
+        values: new ArrayVector(),
+        config: { displayName: 'Requests count' },
+      };
+      
+      let ids = ["A", "B"];
+      let titles = ["test1", "test2"];
+      let subtitles = ['sub1', 'sub2'];
+      let arcVal = [1, 1];
+      let secstat = ["sec stat 1", "sec stat 2"];
+      let sucess = [0, 1];
+      let throttle = [0, 0]
+      let erros = [0, 0];
+      let thr = [0, 0]
+
+      let ids2 = ['1', '2'];
+      let source = ['A', 'B'];
+      let sourceName = ['test1', 'test2'];
+      let target = ['B', 'A'];
+      let targetName = ['test3', 'test4']
+      let mainstat2 = ['edge1', 'edge2'];
+      let secstatedge = ['test34', 'test43']
+
+
+      for(let i = 0; i < ids.length; i++){
+        idField.values.add(ids[i]);
+        titleField.values.add(titles[i]);
+        typeField.values.add(subtitles[i]);
+        mainStatField.values.add(arcVal[i]);
+        secondaryStatField.values.add(secstat[i]);
+        successField.values.add(sucess[i]);
+        faultsField.values.add(throttle[i]);
+        errorsField.values.add(erros[i]);
+        throttledField.values.add(thr[i]);
+
+        edgeIdField.values.add(ids2[i]);
+        edgeSourceField.values.add(source[i]);
+        edgeSourceNameField.values.add(sourceName[i]);
+        edgeTargetField.values.add(target[i]);
+        edgeTargetNameField.values.add(targetName[i]);
+        edgeMainStatField.values.add(mainstat2[i]);
+        edgeSecondaryStatField.values.add(secstatedge[i])
+      }
+
+      return [
+        new MutableDataFrame({
+          name: 'nodes',
+          refId: query?.refId,
+          fields: [
+            idField,
+            titleField,
+            typeField,
+            mainStatField,
+            secondaryStatField,
+            successField,
+            faultsField,
+            errorsField,
+            throttledField,
+          ],
+          meta: {
+            preferredVisualisationType: 'nodeGraph',
+          },
+        }),
+        new MutableDataFrame({
+          name: 'edges',
+          refId: query?.refId,
+          fields: [
+            edgeIdField,
+            edgeSourceField,
+            edgeSourceNameField,
+            edgeTargetField,
+            edgeTargetNameField,
+            edgeMainStatField,
+            edgeSecondaryStatField,
+          ],
+          meta: {
+            preferredVisualisationType: 'nodeGraph',
+          },
+        }),
+      ];
+
+    })
+    return Promise.all(promises).then((data) => ({ data }))
+  }
+
+  // Code for topology
+  /*async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
     const data: QueryResponse[] = [
       {
         columns: [
@@ -85,6 +272,7 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     ]
     return { data };
   }
+  */
 
   async testDatasource() {
     // Implement a health check for your data source.
